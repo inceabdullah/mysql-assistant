82d8b1ad4eb038451ab0f13dc9e52f946018c7cc
inceabdullah <pax.castrensis@gmail.com>
2020-05-17 18:34:05 +0300

    added: DBgetVal

---
+++ DBgetVal.js
@@ -0,0 +1,17 @@
+const sendQuery = require('./DBsendQuery');
+module.exports = (table, columNameKnown, valueKnown, columnWillBeGot) => {
+    return new Promise(resolve => {
+        sendQuery(`select ${columnWillBeGot} from ${table} where ${columNameKnown} = '${valueKnown}';`).then(result => {
+
+            if (result.result == false){
+                resolve(false);
+            }
+            result.result = JSON.stringify(result.result);
+            result.result = JSON.parse(result.result);
+            result.result = Object.values(result.result[0])[0];
+            result.connection.end();
+            resolve(result.result);
+        });
+    });
+
+}
