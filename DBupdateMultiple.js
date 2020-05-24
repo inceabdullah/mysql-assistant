6676eb644e1cbc5c27f6faf969258aa041bd98cc
inceabdullah <pax.castrensis@gmail.com>
2020-05-24 06:10:42 +0000

    drop Column name Syntax Error in MySQL in DBupdateMultiple

--- DBupdateMultiple.js
+++ DBupdateMultiple.js
@@ -1,7 +1,7 @@
 const sendQuery = require('./DBsendQuery');
 module.exports = (table, columName_sKnown, value_sKnown, columnName_sWillChangeValue, newValues) => {
     return new Promise(resolve => {
-        sendQuery(`update ${table} set ${columnName_sWillChangeValue.map(x => x + ' = ?').join(', ')} where ${columName_sKnown.map(x => x + ' = ?').join(' and ')};`,
+        sendQuery(`update ${table} set ${columnName_sWillChangeValue.map(x => `\`${x}\` = ?`).join(', ')} where ${columName_sKnown.map(x => x + ' = ?').join(' and ')};`,
         newValues.concat(value_sKnown)
         ).then(result => {
 
