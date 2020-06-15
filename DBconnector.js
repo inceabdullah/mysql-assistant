2832d3afd1befaf8ec776482de319856efde3e5c
inceabdullah <pax.castrensis@gmail.com>
2020-06-15 08:38:23 +0000

    MySQL connection type is migrated from createConnect() createPool()

--- DBconnector.js
+++ DBconnector.js
@@ -1,3 +1,4 @@
 const mysql      = require('mysql');
 const Creadential = require('./DBcredential.json');
-module.exports = mysql.createConnection(Creadential);
+// module.exports = mysql.createConnection(Creadential);
+module.exports = mysql.createPool(Creadential);
--- DBsendQuery.js
+++ DBsendQuery.js
@@ -1,16 +1,18 @@
-//const connection = require('./DBconnector');
-const mysql   = require('mysql');
-const Creadential = require('./DBcredential.json');
+const connection = require('./DBconnector'); // pool
+// const mysql   = require('mysql');
+// console.log("connection fromDBsendQuery:", connection);
+// const Creadential = require('./DBcredential.json');
 
 module.exports = (query, queryPlaceHolders) => {
     return new Promise(resolve => {
-        const connection = mysql.createConnection(Creadential);
-        connection.connect(function(err) {
+        // const connection = mysql.createConnection(Creadential);
+/*         connection.getConnection(function(err, connection) {
             if (err) {
                 console.error('Error connecting: ' + err);
+                const fakeConnection = {end: () => {return connection.release();} }
                 resolve({
                     result: false,
-                    connection: connection
+                    connection: fakeConnection
                 });
             }
         
@@ -23,13 +25,33 @@ module.exports = (query, queryPlaceHolders) => {
             }
             //connection.end();
             // console.log("results:", results);
+            const fakeConnection = {end: () => {return connection.release();} }
             resolve({
                 result: results,
-                connection: connection
+                connection: fakeConnection
             });
         
         });
+        }); */
+
+
+        connection.query(query, queryPlaceHolders, function (error, results, fields) {
+            if (error){
+                resolve(false);
+                throw error;
+            }
+            //connection.end();
+            // console.log("results:", results);
+            const fakeConnection = {end: () => {return;} }
+            resolve({
+                result: results,
+                connection: fakeConnection // connection
+            });
+        
         });
+
+
+
     }) 
     
 }
