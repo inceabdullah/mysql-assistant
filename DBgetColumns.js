b3adfc0f5137bd49b4979f67a109a10c6f64d4cb
inceabdullah <pax.castrensis@gmail.com>
2020-08-11 06:24:05 -0600

    Added: feature unSafe for DBgetColumns Where left var

--- DBgetColumns.js
+++ DBgetColumns.js
@@ -1,5 +1,5 @@
 const sendQuery = require('./DBsendQuery');
-module.exports = (table, columNameKnown_or_s, valueKnown_or_s, column_sWillBeGot, QUERY_group_by, QUERY_order_by, QUERY_limit, QUERY_WHERE_bt_or_lt) => {
+module.exports = (table, columNameKnown_or_s, valueKnown_or_s, column_sWillBeGot, QUERY_group_by, QUERY_order_by, QUERY_limit, QUERY_WHERE_bt_or_lt, QUERY_WHERE_isUnsafe) => {
     if (!Array.isArray(columNameKnown_or_s)){
         columNameKnown_or_s = [columNameKnown_or_s];
     }
@@ -22,7 +22,7 @@ module.exports = (table, columNameKnown_or_s, valueKnown_or_s, column_sWillBeGot
         console.log({QUERY_WHERE_bt_or_lt});
         sendQuery(`select ${column_sWillBeGot.join(', ')} from ${table} where ${
             columNameKnown_or_s.filter((columnName, columnNameIndex) => ( Array.isArray(valueKnown_or_s) ? valueKnown_or_s : [valueKnown_or_s] )[columnNameIndex] != null).map(columnNameItem => {
-                return `\`${columnNameItem}\` ${QUERY_WHERE_bt_or_lt ? ( QUERY_WHERE_bt_or_lt.find(_=> _[0] == columnNameItem ) ? ( 
+                return `${ (QUERY_WHERE_isUnsafe == true) ? '' : '\`' }${columnNameItem}${ (QUERY_WHERE_isUnsafe == true) ? '' : '\`' } ${QUERY_WHERE_bt_or_lt ? ( QUERY_WHERE_bt_or_lt.find(_=> _[0] == columnNameItem ) ? ( 
                     ( ( (QUERY_WHERE_bt_or_lt.find(_=> _[0] == columnNameItem )[1]) == "bt" ) ? '>' : 
                         ( ( (QUERY_WHERE_bt_or_lt.find(_=> _[0] == columnNameItem )[1]) == "lt" ) ? '<' : '' )
                     )
